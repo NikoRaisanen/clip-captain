f75609a96f3a2db20b902273dddff0c685c06b0a
Niko Raisanen <nraisane@asu.edu>
2021-07-04 10:52:36 -0700

    second commit

---
+++ main.py
@@ -0,0 +1,27 @@
+#!/bin/python
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+# Site to download clips: https://clipr.xyz/ 
+
+# Get url of 20+ valorant clips
+# Individual links found within the hr
+# clipsList = []
+# clipsUrl = 'https://www.twitch.tv/directory/game/VALORANT/clips?range=7d'
+# clipsUrlText = requests.get(clipsUrl).text
+# print(clipsUrlText)
+print("Hello")
+
+# driver = webdriver.Firefox()
+# driver.get('http://eve-central.com/home/quicklook.html?typeid=34')
+
+# html = driver.page_source
+# soup = BeautifulSoup(html)
+
+# # check out the docs for the kinds of things you can do with 'find_all'
+# # this (untested) snippet should find tags with a specific class ID
+# # see: http://www.crummy.com/software/BeautifulSoup/bs4/doc/#searching-by-css-class
+# for tag in soup.find_all("a", class_="my_class"):
+#     print tag.text
+
+
