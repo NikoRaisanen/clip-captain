3f6584a10b200d2c9eb59421da6d1ee53f129114
Niko <nraisane@asu.edu>
2021-07-05 20:43:22 -0700

    twitch api clip download

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #!/bin/python
-import requests, time, json
+import requests, time, json, pprint
 from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
 from selenium import webdriver
@@ -7,33 +7,6 @@ from selenium import webdriver
 
 credentials = []
 # Get url of 20 valorant clips
-# """Without loading dynamic content the following code pulls
-# the url of 20 clips. If more clips are needed, make the selenium webdriver 
-# scroll to the bottom of the page to load dynamic content"""
-clipsList = []
-clipsUrl = 'https://www.twitch.tv/directory/game/VALORANT/clips?range=7d'
-driver = webdriver.Firefox(executable_path='./geckodriver')
-
-driver.get(clipsUrl)
-time.sleep(5)
-html = driver.page_source
-driver.close()
-
-soup = BeautifulSoup(html, 'html.parser')
-
-for link in soup.find_all('a'):
-    if '/clip/' in link.get('href'):
-        clipsList.append('twitch.tv' + link.get('href'))
-            
-# Remove duplicate links
-clipsList = list(dict.fromkeys(clipsList))
-
-# GETTING A LIST OF CLIP URLS COMPLETE
-print('*' * 20 + 'program finished' + '*' * 20)
-print(f'Here is the list of clips:\n {clipsList}')
-print(len(clipsList))
-for item in clipsList:
-    print(item)
 
 # Get credentials for Twitch api from file
 with open('confidential.json', 'r') as fp:
@@ -57,9 +30,24 @@ startDate = startDate + "Z"
 # 2021-06-28T10:53:47Z
 # Use startData var for started_at query parameter for twitch clip api calls
 
+clipDownloadLinks = []
+clipUrls = []
 clipsAPI = 'https://api.twitch.tv/helix/clips'
 PARAMS = {'game_id': 516575, 'started_at': '2021-06-30T10:49:29Z'}
 HEADERS = {'Client-Id': credentials['twitch_client_id'], 'Authorization': 'Bearer ' + credentials['access_bearer_token']}
 r = requests.get(url=clipsAPI, params=PARAMS, headers=HEADERS)
 data = r.json()
-print(data)
+pprint.pprint(data)
+print(type(data))
+
+# Get link to download each video and put into list
+for entry in data['data']:
+    clipDownloadLinks.append(entry['thumbnail_url'])
+
+print(clipDownloadLinks)
+print(len(clipDownloadLinks))
+
+for link in clipDownloadLinks:
+    clipUrls.append(link.split('-preview-')[0] + '.mp4')
+    
+print(clipUrls)
--- testing.py
+++ testing.py
@@ -1,5 +1,6 @@
-import time
+import time, json
 from datetime import datetime, timedelta
+import requests
 
 # Get current time in RFC3339 format with T and Z
 timeNow = datetime.now().isoformat()
@@ -14,3 +15,12 @@ startDate = startDate + "Z"
 # Example of startDate variable:
 # 2021-06-28T10:53:47Z
 # Use startData var for started_at query parameter for twitch clip api calls
+credentials = []
+with open('confidential.json', 'r') as fp:
+    credentials = json.load(fp)
+
+HEADERS = {'Client-Id': credentials['twitch_client_id'], 'Authorization': 'Bearer ' + credentials['access_bearer_token']}
+r = requests.get('https://clips-media-assets2.twitch.tv/AT-cm%7C1239511811.mp4', allow_redirects=True)
+
+with open('clip1.mp4', 'wb') as fp:
+    fp.write(r.content)
