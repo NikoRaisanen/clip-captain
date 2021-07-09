adc8d6874a32c4079f455dcdcc9d35c6a9c2a5e8
Niko <nraisane@asu.edu>
2021-07-08 21:26:31 -0700

    video with text overlay

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 #!/bin/python
-from types import MethodDescriptorType
 import requests, time, json, pprint, os
 from datetime import datetime, timedelta
 from moviepy.editor import *
-
-# Site to download clips: https://clipr.xyz/ 
 
 # Get url of 20 valorant clips
 
@@ -51,6 +48,7 @@ def download_clips(clips):
     # [0] is download url
     # [1] is streamer name
     # [2] is game id
+    streamers = []
     counter = 0    
     for entry in clips:
         if entry[2] == '516575':
@@ -63,29 +61,47 @@ def download_clips(clips):
         r = requests.get(entry[0], allow_redirects=True)
         with open('./clips/' + filename, 'wb') as fp:
             fp.write(r.content)
+
+        streamers.append(entry[1])
+        # Return list of streamer names (give credit in youtube description)
+        # Set conversion used to remove duplicates
+    return list(set(streamers))
+
 # takes list of clips and combines them
 def combine_clips(clips):
     videoObjects = []
     for clip in clips:
+        # Add text below:
         video = VideoFileClip('clips/' + clip, target_resolution=(1080,1920))
+        txt_clip = TextClip("Chronodotafan123", fontsize = 60, color = 'white',stroke_color='black',stroke_width=2, font="Fredoka-One")
+        txt_clip = txt_clip.set_pos((0.7, 0.85), relative=True).set_duration(video.duration)
+        video = CompositeVideoClip([video, txt_clip]).set_duration(video.duration)
         videoObjects.append(video)
 
+    # Make transition clip 1 second long and halve the volume
     transition = VideoFileClip('tvstatictransition.mp4').fx(afx.volumex, 0.5)
     transition = transition.subclip(0, -1)
     # video name based on game
     videoName = clips[0].split('Clip')[0] + '.mp4'
     final = concatenate_videoclips(videoObjects, transition=transition, method='compose')
+    # final = concatenate_videoclips(videoObjects, transition=transition, method='compose')
     final.write_videofile(videoName, fps=60, bitrate="6000k")
 
 def main():
     # BEGIN GETTING + DOWNLOADING CLIPS
-    credentials = get_credentials()
-    clips = get_clip_info(credentials)
-    download_clips(clips)
+    # credentials = get_credentials()
+    # clips = get_clip_info(credentials)
+    # streamers = download_clips(clips)
+    # print(f'Here are the streamers featured in this weeks episode: {streamers}')
     # END GETTING + DOWNLOADING CLIPS
 
-    # BEGIN JOINING CLIPS TOGETHER
-    combine_clips(os.listdir('clips'))
+    # Join clips together, writes an mp4 file in the cwd
+    allClips = os.listdir('clips')
+    combine_clips(allClips[0:2])
+    fonts = TextClip.list('font')
+    print(fonts)
+    # for font in fonts:
+    #     print(font)
     # Read about compose method on line 30:
     # https://github.com/Zulko/moviepy/blob/master/moviepy/video/compositing/concatenate.py
 if __name__ == "__main__":
