6421bb6a6cc2536bd35921172da48f708d7bc5f9
Niko <nraisane@asu.edu>
2021-08-14 21:30:06 -0700

    OO working up to and including combine_clips()

--- ObjectOriented.py
+++ ObjectOriented.py
@@ -13,10 +13,10 @@ import os
 import socket
 
 # Data structure for the final video, used to populate information in Youtube Upload API
-class Video:
-    def __init__(self, gameId, videoName, videoTitle, streamers, thumbnail, tags, description):
-        self.gameId = gameId
-        self.videoName = videoName
+class VideoObj:
+    def __init__(self, gameName, filename, streamers=[], videoTitle='', thumbnail=None, tags=[], description=''):
+        self.gameId = gameName
+        self.filename = filename
         self.videoTitle = videoTitle
         self.streamers = streamers
         self.thumbnail = thumbnail
@@ -28,6 +28,7 @@ class Video:
 
 # Data structure for each individual clip
 class Clip:
+    gameName = ''
     def __init__(self, downloadLink, streamerName, filename):
         self.downloadLink = downloadLink
         self.streamerName = streamerName
@@ -39,8 +40,23 @@ def get_credentials():
         credentials = json.load(fp)
         return credentials
 
+# Possibly use dropdown on front-end to get the gameName
+def get_game_id(gameName, credentials):
+    gamesAPI = 'https://api.twitch.tv/helix/games'
+    PARAMS = {'name': gameName}
+    HEADERS = {'Client-Id': credentials['twitch_client_id'], 'Authorization': 'Bearer ' + credentials['access_bearer_token']}
+    r = requests.get(url=gamesAPI, params=PARAMS, headers=HEADERS)
+    data = r.json()
+
+    if data['data'] == []:
+        # SHOW THIS TO USER WHEN THEY TRY TO SUBMIT
+        raise Exception(f'*****COULD NOT RESOLVE GAMEID FOR {gameName}*****')
+    else:
+        gameId = data['data'][0]['id']
+        Clip.gameName = gameName
 
 
+    return gameId
 
 # game_id, pastDays, numClips, language <-- all vars declared on program start
 def get_clip_info(credentials, game_id, pastDays=7, numClips = 20, first = 50, cursor = None, language =None):
@@ -62,9 +78,7 @@ def get_clip_info(credentials, game_id, pastDays=7, numClips = 20, first = 50, c
     # Call twitch api to get thumbnail url, broadcaster name, game id
     counter = 0
     clips = []
-    # clipInfo = []
-    # finalClipInfo = []
-    # previousClips = []
+
 
     # Keep paginating through twitch clips API until 20 valid clips
     while len(clips) < numClips:
@@ -77,25 +91,6 @@ def get_clip_info(credentials, game_id, pastDays=7, numClips = 20, first = 50, c
         r = requests.get(url=clipsAPI, params=PARAMS, headers=HEADERS)
         data = r.json()
 
-        # # Keep going through results until 20 clips in spanish are acquired
-        # if language[:2] == 'es':
-        #     try:
-        #         if data['pagination']['cursor']:
-        #             cursor = data['pagination']['cursor']
-        #         else:
-        #             print('executing else block, and breaking')
-        #             break
-        #     except KeyError:
-        #         print('Keyerror found, attempting to break out of loop')
-        #         break
-
-        #     for item in data['data']:
-        #         # Add clip to list if language is Spanish AND broadcaster_name + clip title combination is unique
-        #         if item['language'][:2] == 'es' and previousClips.count([item['broadcaster_name'], item['title']]) == 0 and 'vtube' not in item['broadcaster_name'].casefold():
-        #             clipInfo.append([item['thumbnail_url'], item['broadcaster_name'], item['game_id']])
-        #             previousClips.append([item['broadcaster_name'], item['title']])
-
-
 
         # Take the top 20 clips returned by clips api
         if language == None:
@@ -103,44 +98,81 @@ def get_clip_info(credentials, game_id, pastDays=7, numClips = 20, first = 50, c
                 cursor = data['pagination']['cursor']
                 if len(clips) < numClips:
                     counter += 1
-                    filename = item['game_id'] + item['broadcaster_name'] + str(counter) + '.mp4'
-                    clipObj = Clip(item['thumbnail_url'], item['broadcaster_name'], filename)
+                    filename = Clip.gameName + item['broadcaster_name'] + str(counter) + '.mp4'
+                    downloadLink = item['thumbnail_url'].split('-preview-')[0] + '.mp4'
+                    clipObj = Clip(downloadLink, item['broadcaster_name'], filename)
                     clips.append(clipObj)
-                    
+
         elif language[:2] == 'en':
             for item in data['data']:
                 cursor = data['pagination']['cursor']
                 if len(clips) < numClips and item['language'] == language[:2]:
                     counter += 1
-                    filename = item['game_id'] + item['broadcaster_name'] + str(counter) + '.mp4'
-                    clipObj = Clip(item['thumbnail_url'], item['broadcaster_name'], filename)
+                    filename = Clip.gameName + item['broadcaster_name'] + str(counter) + '.mp4'
+                    downloadLink = item['thumbnail_url'].split('-preview-')[0] + '.mp4'
+                    clipObj = Clip(downloadLink, item['broadcaster_name'], filename)
                     clips.append(clipObj)
 
-                # clipInfo.append([item['thumbnail_url'], item['broadcaster_name'], item['game_id']])
-                # previousClips.append([item['broadcaster_name'], item['title']])
+    print('Done getting clip info...')
+    return clips
 
-        # cursor = data['pagination']['cursor']
-    # Convert thumbnail_url to download link and create list with download link, broadcaster_name, game_id
 
+def download_clips(clips):
+    counter = 0
+    global downloadPath
+    basePath = os.path.join(os.getcwd(), 'clips')
+    downloadPath = os.path.join(basePath, Clip.gameName)
+    if not os.path.exists(downloadPath):
+        os.mkdir(downloadPath)
+
+    # Create Video Object and define required attributes
+    vidFileName = Clip.gameName + '.mp4'
+    Video = VideoObj(Clip.gameName, vidFileName, [])
 
     for clip in clips:
-        clip.downloadLink = clip.downloadLink.split('-preview-')[0] + '.mp4'
-        print(clip.downloadLink)
+        r = requests.get(clip.downloadLink, allow_redirects=True)
+        Video.streamers.append(clip.streamerName)
+        with open(os.path.join(downloadPath, clip.filename), 'wb') as fp:
+            try:
+                fp.write(r.content)
+                print(f'Downloading clip {str(counter + 1)} of {len(clips)} to {os.path.join(downloadPath, clip.filename)}')
+            except:
+                print("except block executed")
 
-    print('Done getting clip info...')
-    print(clips)
-
-    # for link in clipInfo:
-    #     finalClipInfo.append([link[0].split('-preview-')[0] + '.mp4', link[1], link[2]])
-    # print('done getting clip info')
-    # print(clipInfo)
-    # print(len(clipInfo))
-    # return finalClipInfo, game_id
-
-
+        counter += 1
+    print(f'Finished downloading {counter} clips for {Clip.gameName}!')
+    return Video
 
 
 
+
+
+
+def combine_clips(clips):
+    videoObjects = []
+    for clip in clips:
+        # Add text below:
+        streamerName = clip.streamerName
+        
+        video = VideoFileClip(os.path.join(downloadPath, clip.filename), target_resolution=(1080,1920))
+        txt_clip = TextClip(clip.streamerName, fontsize = 60, color = 'white',stroke_color='black',stroke_width=2, font="Fredoka-One")
+        txt_clip = txt_clip.set_pos((0.8, 0.9), relative=True).set_duration(video.duration)
+        video = CompositeVideoClip([video, txt_clip]).set_duration(video.duration)
+        videoObjects.append(video)
+
+    print('done creating list of video objects')
+    # Make transition clip 1 second long and halve the volume
+    transition = VideoFileClip('assets/tvstatictransition.mp4').fx(afx.volumex, 0.5)
+    transition = transition.subclip(0, -1)
+    # video name based on game
+    videoName = Clip.gameName + '.mp4'
+    print('Beginning to concatenate video clips...')
+    final = concatenate_videoclips(videoObjects, transition=transition, method='compose')
+    # final = concatenate_videoclips(videoObjects, transition=transition, method='compose')
+    final.write_videofile(os.path.join(os.getcwd(), 'finalVideos', videoName), fps=60, bitrate="6000k")
+    return os.path.join(os.getcwd(), 'finalVideos', videoName)
+
+    # END CLIP PROCUREMENT, DOWNLOAD, AND COMBINING
 
 
 
@@ -157,9 +189,21 @@ def get_clip_info(credentials, game_id, pastDays=7, numClips = 20, first = 50, c
 
 
 def main():
-    credentials = get_credentials()
-    get_clip_info(credentials, '29595')
+    beginTime = datetime.now()
+    # Increase socket default timemout due to connection dropping during large file uploads
+    socket.setdefaulttimeout(100000)
 
+    credentials = get_credentials()
+    # See if possible to create dropdown menu for games
+    gameName = 'VALORANT'
+    gameId = get_game_id(gameName, credentials)
+    clips = get_clip_info(credentials, gameId, numClips=3)
+    videoObj = download_clips(clips)
+    vidPath = combine_clips(clips)
+    print(vidPath)
+
+    endTime = datetime.now()
+    print(f'The execution of this script took {(endTime - beginTime).seconds} seconds')
 
 if __name__ == "__main__":
     main()
