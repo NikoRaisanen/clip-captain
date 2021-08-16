94b69a9fc4d7303ce7f43c8759f02ee75d4a56e2
Niko <nraisane@asu.edu>
2021-08-15 20:03:15 -0700

    successfully get form data with flask

--- flaskTest.py
+++ flaskTest.py
@@ -1,9 +1,28 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
 
+UPLOAD_FOLDER = 'thumbnails'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# If GET request, serve index.html
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+    gameName = request.form['gameName']
+    videoTitle = request.form['videoTitle']
+    thumbnail = request.form['thumbnail']
+    privacyStatus = request.form['privacyStatus']
+    tags = request.form['tags']
+    description = request.form['description']
+    info = videoTitle + thumbnail + privacyStatus + tags + description
+    print(info)
+    return info
 if __name__ == '__main__':
     app.run(debug=True)
