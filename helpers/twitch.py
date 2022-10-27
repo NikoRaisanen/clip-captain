8795d916b46dd0504cc675abaab6f3d60cfe22c0
NikoRaisanen <nikoraisanenbusiness@gmail.com>
2022-10-26 20:07:41 -0700

    finished get_clips function. Refactored and tested manually

--- helpers/twitch.py
+++ helpers/twitch.py
@@ -18,13 +18,13 @@ class Video:
         self.description = description
         self.privacy_status = privacy_status
         self.clips = clips
-    
+
     def __str__(self):
         return f'{self.game_name}\n{self.filename}\n{self.title}\n{self.thumbnail}\n{self.tags}\n{self.description}\n{self.streamers}'
 
     def get_unique_streamers(self):
         self.streamers = list(set(self.streamers))
-    
+  
     def set_default_description(self):
         credit = ''
         for streamer in self.streamers:
@@ -32,7 +32,7 @@ class Video:
             credit = f'{credit}\n{link}'
 
         self.description = f'{self.title}\n\nMake sure to support the streamers in the video!\n{credit}'
-            
+          
 
 # Data structure for each individual clip
 class Clip:
@@ -45,7 +45,7 @@ class Clip:
         
 # Possibly use dropdown on front-end to get the gameName
 # Consider printing a mapping of gameId to gameName in console.
-def get_game_id(name, creds):
+def get_game_id(creds, name):
     games_endpoint = 'https://api.twitch.tv/helix/games'
     PARAMS = {'name': name}
     HEADERS = {'Client-Id': creds['client_id'], 'Authorization': f'Bearer {creds["bearer_access_token"]}'}
@@ -54,11 +54,17 @@ def get_game_id(name, creds):
 
     try:
         return resp['data'][0]['id']
-    except Exception as e:
+    except Exception:
         raise KeyError(f'Cannot find game id for {name}, try adding or removing spaces')
 
-# game_id, pastDays, numClips, language <-- all vars declared on program start
-def get_clip_info(creds, game_id=None, past_days=7, num_clips = 20, first = 50, cursor = None, language=None):
+
+# game_id, pastDays, numClips, first, language <-- all vars declared on program start
+# first param must be <= 50
+def get_clip_info(creds=None, game_id=None, past_days=7, num_clips = 20, first = 20, cursor = None, language=None):
+    """
+    Returns list of Clip objects that contains the following info for
+    each video clip: filename, download link, and name of creator
+    """
     # TODO: allow multi-language support
     # TODO: allow a custom date range for clips, not only pastDays
     print(f'Language to be used is: {language}')
@@ -67,41 +73,39 @@ def get_clip_info(creds, game_id=None, past_days=7, num_clips = 20, first = 50, 
     start_date = time_now - datetime.timedelta(past_days)
     start_date = start_date.isoformat('T') + 'Z'
 
-    # Call twitch api to get thumbnail url, broadcaster name, game id
-    counter = 1
+    counter = 0
     clips = []
-
+    clips_per_creator = {}
 
     # Keep paginating through twitch clips API until 20 valid clips
     while len(clips) < num_clips:
+        counter += 1
         print(f'Query #{counter} for valid clips')
         clipsAPI = 'https://api.twitch.tv/helix/clips'
         PARAMS = {'game_id': game_id, 'started_at': start_date, 'first': first, 'after': cursor}
-        HEADERS = {'Client-Id': creds['twitch_client_id'], 'Authorization': 'Bearer ' + creds['access_bearer_token']}
+        HEADERS = {'Client-Id': creds['client_id'], 'Authorization': f'Bearer {creds["bearer_access_token"]}'}
         r = requests.get(url=clipsAPI, params=PARAMS, headers=HEADERS)
         data = r.json()
 
 
         # Take the top 20 clips returned by clips api
-        if language == None:
+        if language is None:
             for item in data['data']:
                 cursor = data['pagination']['cursor']
+                # consider reversing logic. guard clause to break out of loop might be cleaner
                 if len(clips) < num_clips:
-                    counter += 1
-                    filename = Clip.game_name + item['broadcaster_name'] + str(counter) + '.mp4'
-                    downloadLink = item['thumbnail_url'].split('-preview-')[0] + '.mp4'
-                    clipObj = Clip(downloadLink, item['broadcaster_name'], filename)
-                    clips.append(clipObj)
+                    creator = item['broadcaster_name']
+                    if creator not in clips_per_creator:
+                        clips_per_creator[creator] = 1
+                    else:
+                        clips_per_creator[creator] += 1
 
-        elif language[:2] == 'en':
-            for item in data['data']:
-                cursor = data['pagination']['cursor']
-                if len(clips) < num_clips and item['language'] == language[:2]:
-                    counter += 1
-                    filename = Clip.game_name + item['broadcaster_name'] + str(counter) + '.mp4'
-                    downloadLink = item['thumbnail_url'].split('-preview-')[0] + '.mp4'
-                    clipObj = Clip(downloadLink, item['broadcaster_name'], filename)
-                    clips.append(clipObj)
+                    filename = f'{creator}{clips_per_creator[creator]}.mp4'
+                    download_link = f'{item["thumbnail_url"].split("-preview-")[0]}.mp4'
+                    clip = Clip(download_link, creator, filename)
+                    clips.append(clip)
+        else:
+            raise Exception('Language options are not yet supported in this program')
 
     print('Done getting clip info...')
     return clips
@@ -132,17 +136,18 @@ def get_clip_info(creds, game_id=None, past_days=7, num_clips = 20, first = 50, 
 #     print(f'Here are the unique streamers:\n{videoStruct.streamers}')
 #     print(f'Finished downloading {counter} clips for {Clip.gameName}!')
 
-def get_clips(creds, game_name, past_days, num_clips, first):
+def get_clips(creds=None, game_name=None, past_days=7, num_clips=20, first=20):
     """Wrapper function to perform all helpers"""
     # TODO: This should return array of Clip objects
     # Video object will be created later
     # vid = Video()
     game_id = get_game_id(creds, game_name)
-    get_clip_info(creds, game_id, past_days, num_clips, first)
-    return
+    clips = get_clip_info(creds, game_id, past_days, num_clips, first)
+    return clips
 
 
 if __name__ == '__main__':
     creds = auth.get_credentials()
-    game_id = get_game_id('dota 2', creds)
-    get_clip_info()
+    clips = get_clips(creds, game_name='dota 2')
+    print(clips)
+    print(len(clips))
